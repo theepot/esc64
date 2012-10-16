9f4dbb62bb4f0e78be4357140614df1b7c0a4839
Theepot <lukas_vermond@hotmail.com>
2012-10-16 13:55:11 +0200

    updated CPU simulation

--- sim/ir/ir_test.v
+++ sim/ir/ir_test.v
@@ -1,31 +1,38 @@
 `include "ir.v"
 
+//module instructionRegister(clock, notReset, notLoad, OE, in, outOpcode, outOp0, outOp1, outOp2);
+
 module ir_test();
 	reg [15:0] data;
+	reg dataE;
 	wire [6:0] opcode;
 	wire [2:0] op0, op1, op2;
-	reg clock, output_enable, load;
+	reg clock, notReset, notLoad, OE;
 	
-	assign data_bus = oe ? data : 16'bzzzzzzzzzzzzzzzz;
-	
+	assign data_bus = dataE ? data : 16'bzzzzzzzzzzzzzzzz;
 	
 	initial begin
 		$dumpfile("wave.vcd");
-		$dumpvars(0, clock, output_enable, load, data, opcode, op0, op1, op2);
+		$dumpvars(0, data, opcode, op0, op1, op2, clock, notReset, notLoad, OE);
 	
+		dataE = 0;
 		clock = 0;
-		output_enable = 0;
-		load = 0;
+		notReset = 1;
+		notLoad = 1;
+		OE = 1;
 		data = 0;
 		
-		#5 output_enable = 1;
-
-		#20 output_enable = 0;
+		#20	OE = 1;
+		#20	OE = 0;
 		
-		#10 data = 16'HF0F0;
-		#10 load = 1;
-		#20 load = 0;
-		#10 output_enable = 1;
+		#20 data = 16'HF0F0;
+		#20	dataE = 1;
+		#20 notLoad = 0;
+		#20 notLoad = 1;
+		#20	dataE = 0;
+		
+		#20	OE = 1;
+		#20	OE = 0;
 		
 		#50 $finish;
 	end
@@ -34,6 +41,6 @@ module ir_test();
 		#5 clock = ~clock;
 	end
 	
-	instructionRegister ir(clock, output_enable, load, data, opcode, op0, op1, op2);
+	instructionRegister ir(clock, notReset, notLoad, OE, data, opcode, op0, op1, op2);
 
 endmodule
