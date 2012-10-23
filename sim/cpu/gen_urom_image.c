1ab734766d89c7a17e6cf1582eb6bcc32070b07b
Kasper <defusix@defusix.net>
2012-10-23 13:49:42 +0200

    added cpu.h, which now contains most definitions that were first in gen_urom_.c

--- sim/cpu/gen_urom_image.c
+++ sim/cpu/gen_urom_image.c
@@ -1,72 +1,35 @@
 #include <uassembler.h>
-#include "opcodes.h"
-
-#define ADDR_WIDTH			13
-#define DATA_WIDTH			(ADDR_WIDTH + 27 + 1)
-#define OPCODE_WIDTH 		7
-
-#define RGS_GP0				"0"
-#define RGS_GP1				"1"
-#define RGS_GP2				"2"
-#define RGS_GP3				"3"
-#define RGS_GP4				"4"
-#define RGS_LR				"5"
-#define RGS_SP				"6"
-#define RGS_PC				"7"
-
-#define RGS_LOADSRC_USEQ	"0"
-#define RGS_LOADSRC_OP0 	"1"
-#define RGS_OESRC_USEQ 		"0"
-#define RGS_OESRC_OP0 		"1"
-#define RGS_OESRC_OP1 		"2"
-#define RGS_OESRC_OP2 		"3"
-
-#define ALU_F_A				"0"
-#define ALU_F_B				"21"
-#define ALU_F_SUB			"12"
-#define ALU_F_ADD			"18"
-#define ALU_F_NOT			"1"
-#define ALU_F_XOR			"15"
-#define ALU_F_AND			"23"
-#define ALU_F_OR			"29"
-#define ALU_F_SHIFT_LEFT	"1"
-#define ALU_F_SHIFT_RIGHT	"0"
-
-#define ALU_FSEL_74181		"0"
-#define ALU_FSEL_SHIFT		"1"
-
-#define ALU_CSEL_UCIN		"0"
-#define ALU_CSEL_FCIN		"1"
+#include "cpu.h"
 
 field_description field_descrps[] = {
 	{.name = "nextsel", .index = 0, .width = 1, .active = 1},
-	{.name = "next", .index = 1, .width = ADDR_WIDTH, .active = 1},
-	{.name = "statusNotLoad", .index = (26 + 1 + ADDR_WIDTH), .width = 1, .active = L},
-	{.name = "regselOE", .index = (25 + 1 + ADDR_WIDTH), .width = 1, .active = H},
-	{.name = "regselLoad", .index = (24 + 1 + ADDR_WIDTH), .width = 1, .active = H},
-	{.name = "regselOESourceSel", .index = (22 + 1 + ADDR_WIDTH), .width = (23-22+1), .active = H},
-	{.name = "regselLoadSourceSel", .index = (21 + 1 + ADDR_WIDTH), .width = 1, .active = H},
-	{.name = "regselUSeqRegSelOE", .index = (18 + 1 + ADDR_WIDTH), .width = (20-18+1), .active = H},
-	{.name = "regselUSeqRegSelLoad", .index = (15 + 1 + ADDR_WIDTH), .width = (17-15+1), .active = H},
-	{.name = "pcInc", .index = (14 + 1 + ADDR_WIDTH), .width = 1, .active = H},
-	{.name = "aluBRegNotLoad", .index = (13 + 1 + ADDR_WIDTH), .width = 1, .active = L},
-	{.name = "aluYOE", .index = (12 + 1 + ADDR_WIDTH), .width = 1, .active = H},
-	{.name = "aluF", .index = (7 + 1 + ADDR_WIDTH), .width = (11-7+1), .active = H},
-	{.name = "aluFSel", .index = (6 + 1 + ADDR_WIDTH), .width = 1, .active = H},
-	{.name = "aluCSel", .index = (5 + 1 + ADDR_WIDTH), .width = 1, .active = H},
-	{.name = "aluUCIn", .index = (4 + 1 + ADDR_WIDTH), .width = 1, .active = H},
-	{.name = "memNotOE", .index = (3 + 1 + ADDR_WIDTH), .width = 1, .active = L},
-	{.name = "memNotWE", .index = (2 + 1 + ADDR_WIDTH), .width = 1, .active = L},
-	{.name = "memNotCS", .index = (1 + 1 + ADDR_WIDTH), .width = 1, .active = L},
-	{.name = "irNotLoad", .index = (0 + 1 + ADDR_WIDTH), .width = 1, .active = L}
+	{.name = "next", .index = 1, .width = UROM_ADDR_WIDTH, .active = 1},
+	{.name = "statusNotLoad", .index = (26 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = L},
+	{.name = "regselOE", .index = (25 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = H},
+	{.name = "regselLoad", .index = (24 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = H},
+	{.name = "regselOESourceSel", .index = (22 + 1 + UROM_ADDR_WIDTH), .width = (23-22+1), .active = H},
+	{.name = "regselLoadSourceSel", .index = (21 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = H},
+	{.name = "regselUSeqRegSelOE", .index = (18 + 1 + UROM_ADDR_WIDTH), .width = (20-18+1), .active = H},
+	{.name = "regselUSeqRegSelLoad", .index = (15 + 1 + UROM_ADDR_WIDTH), .width = (17-15+1), .active = H},
+	{.name = "pcInc", .index = (14 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = H},
+	{.name = "aluBRegNotLoad", .index = (13 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = L},
+	{.name = "aluYOE", .index = (12 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = H},
+	{.name = "aluF", .index = (7 + 1 + UROM_ADDR_WIDTH), .width = (11-7+1), .active = H},
+	{.name = "aluFSel", .index = (6 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = H},
+	{.name = "aluCSel", .index = (5 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = H},
+	{.name = "aluUCIn", .index = (4 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = H},
+	{.name = "memNotOE", .index = (3 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = L},
+	{.name = "memNotWE", .index = (2 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = L},
+	{.name = "memNotCS", .index = (1 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = L},
+	{.name = "irNotLoad", .index = (0 + 1 + UROM_ADDR_WIDTH), .width = 1, .active = L}
 };
 
 int main(int argc, char** argv)
 {
 	field_descriptions = field_descrps;
 	fields_amount = sizeof(field_descrps) / sizeof(field_description);
-	addr_width = ADDR_WIDTH;
-	data_width = DATA_WIDTH;
+	UROM_ADDR_WIDTH = UROM_ADDR_WIDTH;
+	data_width = UROM_DATA_WIDTH;
 	opcode_width = OPCODE_WIDTH;
 	
 	init();
