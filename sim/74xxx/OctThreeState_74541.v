40df4125d3d90d09502da0d3eb981b53f39e9f56
Theepot <lukas_vermond@hotmail.com>
2012-10-25 11:33:15 +0200

    added buffer and renamed / moved 74xx chips

---
+++ sim/74xxx/OctThreeState_74541.v
@@ -0,0 +1,18 @@
+`ifndef OCT_THREE_STATE_74541_INCLUDED
+`define OCT_THREE_STATE_74541_INCLUDED
+
+module OctThreeState_74541(notOE1, notOE2, in, out);
+	input notOE1, notOE2;
+	wire notOE1, notOE2;
+	
+	input[7:0] in;
+	wire[7:0] in;
+	
+	output[7:0] out
+	wire[7:0] out;
+
+	assign #38 out = ~(notOE1 | notOE2) ? in : 8'bzzzzzzzz;
+endmodule
+
+`endif
+
