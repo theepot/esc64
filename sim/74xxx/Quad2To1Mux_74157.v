e0081ff59355623833ea015745e7be3375f685d4
Kasper <defusix@defusix.net>
2012-10-29 17:01:26 +0100

    Fixed mistake in Quad2To1Mux_74157.

--- sim/74xxx/Quad2To1Mux_74157.v
+++ sim/74xxx/Quad2To1Mux_74157.v
@@ -1,14 +1,14 @@
 `ifndef QUAD_2_TO_1_MUX_74157_INCLUDED
 `define QUAD_2_TO_1_MUX_74157_INCLUDED
 
-module Quad2To1Mux74157(notIE, sel, inA, inB, out);
+module Quad2To1Mux_74157(notIE, sel, inA, inB, out);
 	input notIE;
 	input sel;
 	output[3:0] out;
 	input[3:0] inA;
 	input[3:0] inB;
 
-	assign #25 out = !notIE ? (sel ? inA : inB) : 4'b0000;
+	assign #25 out = !notIE ? (sel ? inB : inA) : 4'b0000;
 
 endmodule
 
