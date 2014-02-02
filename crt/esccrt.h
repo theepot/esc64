2785fddba4a15a42e7eb850c147d1b09451eef9c
theepot <lukas_vermond@hotmail.com>
2014-02-02 22:48:43 +0100

    Update esccrt.h
    fixed prototype of cmemcpy
    committed using web interface :D

--- crt/esccrt.h
+++ crt/esccrt.h
@@ -2,7 +2,7 @@
 #define ESCCRT_INCLUDED
 
 void __exit(int code);
-void __cmemcpy(void* dest, const void* src, unsigned n);
+void* __cmemcpy(void* dest, const void* src, unsigned n);
 void __assert_fail(void);
 
 #endif
